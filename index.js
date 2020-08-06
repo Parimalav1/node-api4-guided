4c908b55b92e0c332b7df06235e101f063f23007
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-08-06 14:53:48 -0700

    deploying

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
-const port = 5000;
+server.get('/', (req,res) => {
+  const message = process.env.MESSAGE || "Hello from guided project";
+  res.status(200).json({message, database: process.env.DB_NAME});
+});
+
+const port = process.env.PORT || 5000;
+  
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
